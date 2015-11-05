5bd95d703d40870628a75f45c028548560504cd2
Alexandr Akulich <akulichalexander@gmail.com>
2015-11-06 01:32:40 +0500

    xrCore/_std_extensions.h: _valid() reimplemented via std.

--- src/xrCore/_std_extensions.h
+++ src/xrCore/_std_extensions.h
@@ -1,6 +1,8 @@
 #ifndef _STD_EXT_internal
 #define _STD_EXT_internal
 
+#include <cmath>
+
 #define BREAK_AT_STRCMP
 #ifndef DEBUG
 #undef BREAK_AT_STRCMP
@@ -112,18 +114,7 @@ IC float _sin(float x) { return sinf(x); }
 IC float _cos(float x) { return cosf(x); }
 IC BOOL _valid(const float x)
 {
-    // check for: Signaling NaN, Quiet NaN, Negative infinity ( –INF), Positive infinity (+INF), Negative denormalized, Positive denormalized
-    int cls = _fpclass(double(x));
-    if (cls&(_FPCLASS_SNAN + _FPCLASS_QNAN + _FPCLASS_NINF + _FPCLASS_PINF + _FPCLASS_ND + _FPCLASS_PD))
-        return false;
-
-    /* *****other cases are*****
-    _FPCLASS_NN Negative normalized non-zero
-    _FPCLASS_NZ Negative zero ( – 0)
-    _FPCLASS_PZ Positive 0 (+0)
-    _FPCLASS_PN Positive normalized non-zero
-    */
-    return true;
+    return std::isnormal(x);
 }
 
 
@@ -134,18 +125,7 @@ IC double _sin(double x) { return sin(x); }
 IC double _cos(double x) { return cos(x); }
 IC BOOL _valid(const double x)
 {
-    // check for: Signaling NaN, Quiet NaN, Negative infinity ( –INF), Positive infinity (+INF), Negative denormalized, Positive denormalized
-    int cls = _fpclass(x);
-    if (cls&(_FPCLASS_SNAN + _FPCLASS_QNAN + _FPCLASS_NINF + _FPCLASS_PINF + _FPCLASS_ND + _FPCLASS_PD))
-        return false;
-
-    /* *****other cases are*****
-    _FPCLASS_NN Negative normalized non-zero
-    _FPCLASS_NZ Negative zero ( – 0)
-    _FPCLASS_PZ Positive 0 (+0)
-    _FPCLASS_PN Positive normalized non-zero
-    */
-    return true;
+    return std::isnormal(x);
 }
 
 // int8
