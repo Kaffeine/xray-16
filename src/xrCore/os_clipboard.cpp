8ee774c4337f3f14883bcfa07315cc7cd9af632b
Alexandr Akulich <akulichalexander@gmail.com>
2015-11-08 17:39:57 +0500

    xrCore/os_clipboard: Reimplemented with SDL.

--- src/xrCore/os_clipboard.cpp
+++ src/xrCore/os_clipboard.cpp
@@ -5,28 +5,14 @@
 // Description : os clipboard class implementation
 ////////////////////////////////////////////////////////////////////////////
 
-#include "stdafx.h"
-#pragma hdrstop
+#include "xrCore.h"
 #include "os_clipboard.h"
+
+#include <SDL2/SDL_clipboard.h>
 
 void os_clipboard::copy_to_clipboard(LPCSTR buf)
 {
-    if (!OpenClipboard(0))
-        return;
-    u32 handle_size = (xr_strlen(buf) + 1) * sizeof(char);
-    HGLOBAL handle = GlobalAlloc(GHND, handle_size);
-    if (!handle)
-    {
-        CloseClipboard();
-        return;
-    }
-
-    char* memory = (char*)GlobalLock(handle);
-    xr_strcpy(memory, handle_size, buf);
-    GlobalUnlock(handle);
-    EmptyClipboard();
-    SetClipboardData(CF_TEXT, handle);
-    CloseClipboard();
+    SDL_SetClipboardText(buf);
 }
 
 void os_clipboard::paste_from_clipboard(LPSTR buffer, u32 const& buffer_size)
@@ -34,58 +20,26 @@ void os_clipboard::paste_from_clipboard(LPSTR buffer, u32 const& buffer_size)
     VERIFY(buffer);
     VERIFY(buffer_size > 0);
 
-    if (!OpenClipboard(0))
-        return;
+    char *text = SDL_GetClipboardText();
 
-    HGLOBAL hmem = GetClipboardData(CF_TEXT);
-    if (!hmem)
-        return;
+    xr_strcpy(buffer, buffer_size, text);
 
-    LPCSTR clipdata = (LPCSTR)GlobalLock(hmem);
-    strncpy_s(buffer, buffer_size, clipdata, buffer_size - 1);
-    buffer[buffer_size - 1] = 0;
-    for (u32 i = 0; i < strlen(buffer); ++i)
-    {
-        char c = buffer[i];
-        if (((isprint(c) == 0) && (c != char(-1))) || c == '\t' || c == '\n')// "ÿ" = -1
-        {
-            buffer[i] = ' ';
-        }
-    }
-
-    GlobalUnlock(hmem);
-    CloseClipboard();
+    SDL_free(text);
 }
 
 void os_clipboard::update_clipboard(LPCSTR string)
 {
-    if (!OpenClipboard(0))
-        return;
-
-    HGLOBAL handle = GetClipboardData(CF_TEXT);
-    if (!handle)
-    {
-        CloseClipboard();
-        copy_to_clipboard(string);
+    if (!SDL_HasClipboardText()) {
+        SDL_SetClipboardText(string);
         return;
     }
 
-    LPSTR memory = (LPSTR)GlobalLock(handle);
-    int memory_length = (int)strlen(memory);
-    int string_length = (int)strlen(string);
-    int buffer_size = (memory_length + string_length + 1) * sizeof(char);
-#ifndef _EDITOR
-    LPSTR buffer = (LPSTR)_alloca(buffer_size);
-#else // #ifndef _EDITOR
-    LPSTR buffer = (LPSTR)xr_alloc<char>( buffer_size );
-#endif // #ifndef _EDITOR
-    xr_strcpy(buffer, buffer_size, memory);
-    GlobalUnlock(handle);
-
+    char *text = SDL_GetClipboardText();
+    const int buffer_size = strlen(string) + strlen(text);
+    char buffer[buffer_size];
+    xr_strcpy(buffer, buffer_size, text);
     xr_strcat(buffer, buffer_size, string);
-    CloseClipboard();
     copy_to_clipboard(buffer);
-#ifdef _EDITOR
-    xr_free (buffer);
-#endif // #ifdef _EDITOR
+
+    SDL_free(text);
 }
