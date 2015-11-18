d36af1726193b665d4232241bb09fe9112c68bdd
Alexandr Akulich <akulichalexander@gmail.com>
2015-11-18 18:29:23 +0600

    xrCore/Platform: Removed git garbage.

--- src/xrCore/Platform.h
+++ src/xrCore/Platform.h
@@ -6,7 +6,6 @@
 
 #ifdef _WIN32
 
->>>>>>> ai_cleanup
 #define VC_EXTRALEAN // Exclude rarely-used stuff from Windows headers
 #define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
 #ifndef STRICT
